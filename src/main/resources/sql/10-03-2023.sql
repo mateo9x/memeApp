ebd176c3a474dc0d620289e3feb64aa29b99f7d9
Mateusz <mtpala@interia.pl>
2023-03-12 12:48:30 +0100

    added sample gif meme

--- src/main/resources/sql/10-03-2023.sql
+++ src/main/resources/sql/10-03-2023.sql
@@ -19,4 +19,5 @@ insert into memes (id, title, photo_url, date_created, date_approved, user_id, u
 values (1, 'elo', '/assets/memes/1/meme1.jpg', '2023-03-11 00:00:00', '2023-02-01 00:00:00', 1, 0, 0, true, 'funny,heheszky'),
         (2, 'mem numer 2', '/assets/memes/1/meme2.jpg', '2023-03-10 00:00:00', '2023-02-02 00:00:00', 1, 0, 0, true, 'meme'),
         (3, 'mem numer 3', '/assets/memes/2/battle.jpg', '2023-02-04 00:00:00', '2023-02-04 00:00:00', 2, 0, 0, true, 'funny'),
-        (4, 'mem numer 4', '/assets/memes/2/mem.jpg', '2023-02-10 00:00:00', null, 2, 0, 0, false, null);
+        (4, 'mem numer 4', '/assets/memes/2/mem.jpg', '2023-02-10 00:00:00', null, 2, 0, 0, false, null),
+       (5, 'Michael Jackson', '/assets/memes/2/mjackson.gif', '2023-02-10 00:00:00', null, 2, 0, 0, false, null);
