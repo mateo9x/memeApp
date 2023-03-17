fe9719f85d054df8f787ea98aaa399a9f23702ef
Mateusz <mtpala@interia.pl>
2023-03-17 23:28:40 +0100

    added functionality to edit avatar in profile settings

--- angular/src/app/component/profile/settings/profile-settings.component.ts
+++ angular/src/app/component/profile/settings/profile-settings.component.ts
@@ -7,6 +7,7 @@ import {ToastService} from "../../../service/toast/toast.services";
 import {DialogService} from "primeng/dynamicdialog";
 import {NewPasswordDialogComponent} from "./new-password-dialog/new-password-dialog.component";
 import {LanguageService} from "../../../service/language.service";
+import {FileService} from "../../../service/file.service";
 
 @Component({
   selector: 'profile-settings',
@@ -20,20 +21,24 @@ export class ProfileSettingsComponent implements OnInit {
   form: FormGroup;
   languages = [{label: 'Polski', value: 'pl'},
     {label: 'English', value: 'en'}];
+  userIcon: any;
+  userIconFile: any;
 
   constructor(private userService: UserService, private formService: ProfileSettingsFormService,
               private toastService: ToastService, private dialogService: DialogService,
-              private languageService: LanguageService) {
+              private languageService: LanguageService, private fileService: FileService) {
     this.form = this.formService.getFormGroup();
-
   }
 
   ngOnInit() {
     this.userService.userLogged.subscribe({
       next: (response) => {
-        this.user = response;
-        this.languageService.setUserLanguage(response.language);
-        this.prepareForm();
+        if (response) {
+          this.user = response;
+          this.userIcon = response.iconFile;
+          this.languageService.setUserLanguage(response.language);
+          this.prepareForm();
+        }
       }
     });
   }
@@ -71,18 +76,42 @@ export class ProfileSettingsComponent implements OnInit {
 
   update() {
     this.formService.convertFormToUser(this.form, this.user);
+    if (this.userIconFile && this.userIcon !== this.user.iconFile) {
+      const photoUrl = this.user.photoUrl ? this.user.photoUrl : `profile/${this.user.id}/${this.userIconFile.name}`;
+      this.fileService.saveFile(photoUrl, this.userIconFile).subscribe({});
+      this.user.photoUrl = photoUrl;
+    }
     this.userService.updateUser(this.user).subscribe({
       next: (response) => {
         this.formSaved = true;
         this.user = response;
         this.languageService.setUserLanguage(response.language);
         this.languageService.saveLanguage(response.language);
+        this.userService.setUserLogged(response);
         this.toastService.createSuccessToast(this.languageService.getMessage('profile.settings.formUpdate.success'));
       },
       error: () => {
         this.toastService.createErrorToast(this.languageService.getMessage('profile.settings.formUpdate.error'));
       }
     });
+  }
+
+  getUserPhoto() {
+    return this.fileService.getPhoto(this.userIcon);
+  }
+
+  doFileInput(event: any) {
+    const file = event.target.files[0];
+    if (file.type.includes('image')) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.userIcon = e.target.result.split('base64,')[1];
+      };
+      reader.readAsDataURL(file);
+      this.userIconFile = file;
+    } else {
+      this.toastService.createErrorToast(this.languageService.getMessage('profile.settings.userIconChangeError'))
+    }
   }
 
   isFormValid() {
